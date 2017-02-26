de3c36d60e705d00e359e582ceb3789539e1fdd6
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-02-26 17:57:03 -0500

    Roadmap and checkpoints completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,11 @@
 require "httparty"
 require "kele/errors"
 require "json"
+require "kele/roadmap"
 
 class Kele
     include HTTParty
+    include Roadmap
     def initialize(email, password)
         response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
         @auth_token = response["auth_token"]
@@ -20,7 +22,28 @@ class Kele
         @mentor_availability = JSON.parse(response.body)
     end
 
-    
+    def get_messages(*page)
+        if page.empty?
+            response = self.class.get(base_api_endpoint("message_threads"), headers: { "authorization" => @auth_token })
+            @messages = (1..(response["count"]/10 + 1)).map do |i|
+                self.class.get(base_api_endpoint("message_threads?page=#{i}"), headers: { "authorization" => @auth_token })
+
+            end
+               puts @messages
+
+        else
+            response = self.class.get(base_api_endpoint("message_threads?page=#{page.join.to_i}"), headers: { "authorization" => @auth_token })
+            @messages = JSON.parse(response.body)
+
+        end
+    end
+
+    def create_message(user_id, recipient_id, token, subject, stripped)
+        message_data = {body: {user_id: user_id, recipient_id: recipient_id, token: nil, subject: subject, stripped: stripped}, headers: { "authorization" => @auth_token }}
+        self.class.post(base_api_endpoint("messages"), message_data)
+    end
+
+
     private
 
     def base_api_endpoint(end_point)
